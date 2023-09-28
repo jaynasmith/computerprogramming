88ebff5d0d23f64b61edcc2cfaa137ce09dcec60
Scholar <scholar@paradigm.local>
2023-09-28 13:30:26 -0600

    for loops

--- control.py
+++ control.py
@@ -29,7 +29,7 @@
 
 #for loop
 
-# for i range(1, 11):
+# for i in range(1, 11):
 #     print(i)
 # sports = ['soccer' , 'basketball' , 'hockey' , 'football']
 
--- forloop.py
+++ forloop.py
@@ -19,4 +19,32 @@
 # else:
 #     print("done")
 
-list1 = 
+# vehicles = ['car','cycle','bus' , 'tempo']
+
+# for item in vehicles:
+#     print(item)
+#     if item == 'bus':
+#         break
+
+# vehicles = ['car','cycle','bus' , 'tempo']
+
+# for item in vehicles:
+#     if item == 'bus':
+#         continue
+#     print(item)
+
+# numbers = ['7','3','10','55','35','99','2']
+
+# for item in numbers:
+#     print(item)
+# print("No of items in list are:", len(numbers))
+
+# numbers = (1, 2, 3, 4, 5)
+# total = sum(numbers)
+# print(total)
+
+numbers = [2,5,6,10,15,20,25]
+
+for i in numbers:
+    if i % 5 == 0:
+        print(i)
