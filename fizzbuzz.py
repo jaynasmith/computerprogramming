91abd99ecdb3703fafa0a17a98507873d5f5e8c3
Scholar <scholar@paradigm.local>
2023-10-17 13:32:54 -0600

    fizzbuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,18 @@
+user_number = int(input("Pick a number_"))
+
+for i in range(1,101):
+    if (i % 3 == 0):
+        print("Fizz")
+    if (i % 5 == 0):
+        print("Buzz")
+    if (i % 3 == 0) and (i % 5 == 0):
+        print("FizzBuzz")
+print(i)
+
+if (i % user_number == 0):
+        print("Fuzz")
+
+
+ 
+
+
